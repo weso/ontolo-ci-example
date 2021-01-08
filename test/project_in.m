3fd8a1f6d0b2158ee0e924f998199849b5c5bffa
Pablo Menéndez ◔_◔ <pabloyo97@hotmail.com>
2021-01-08 14:08:38 +0100

    Create project_in.m

---
+++ test/project_in.m
@@ -0,0 +1,7 @@
+
+SPARQL """
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix asio: <http://purl.org/hercules/asio/core#>
+select ?v where {
+ ?v rdf:type asio:EuropeanProject
+}"""@asio:ProjectShape
